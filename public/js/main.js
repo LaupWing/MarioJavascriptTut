0e988005813ace63615aa903bf72e5508fbe646a
LaupWing <loc_strict@hotmail.com>
2020-05-12 14:02:29 +0200

    3

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,12 @@
 import {loadLevel} from './loaders.js';
 import {loadMarioSprite, loadBackgroundSprites} from './sprites.js';
+import Compositor from './Compositor.js';
+import {createSpriteLayer, createBackgroundLayer} from './layers.js';
 
 const canvas = document.querySelector('#screen');
 // Acces the context API to actually draw on the canvas
 const context = canvas.getContext('2d');
 
-function drawBackground(background, context,sprites){
-    background.ranges.forEach(([x1, x2, y1, y2])=>{
-        for(let x =x1; x< x2; ++x){
-            for (let y=y1; y<y2; ++y){
-                sprites.drawTile(background.tile, context, x, y);
-            }
-        }
-    });
-}
 
 Promise
     .all([
@@ -21,15 +14,23 @@ Promise
         loadBackgroundSprites(),
         loadLevel('1-1')
     ])
-    .then(([mario,sprites,level])=>{
-        level.backgrounds.forEach(background=>{
-            drawBackground(background, context, sprites);
-        });
+    .then(([marioSprite,backgroundSprites,level])=>{
+        const comp = new Compositor();
+        const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
+        comp.layers.push(backgroundLayer);
 
         const pos = {
             x: 64,
             y: 64
-        }
+        };
+        const spriteLayer = createSpriteLayer(marioSprite, pos);
+        comp.layers.push(spriteLayer);
 
-        mario.draw('idle', context, pos.x, pos.y)
+        function update(){
+            comp.draw(context);
+            pos.x += 2;
+            pos.y += 2;
+            requestAnimationFrame(update);
+        }
+        update();
     });
